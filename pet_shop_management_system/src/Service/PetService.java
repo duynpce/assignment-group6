d67bc1cd8ec9cb6c2bac3181d470904877b99f7e
duynpce <a23.thvl03@gmail.com>
2025-10-22 18:50:02 +0700

    very final version

--- pet_shop_management_system/src/Service/PetService.java
+++ pet_shop_management_system/src/Service/PetService.java
@@ -1,9 +1,5 @@
 package Service;
 
-import java.io.EOFException;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.util.HashMap;
 
 import Enum.Constant;
@@ -15,18 +11,6 @@ public interface PetService {
     public static HashMap<Integer,Pet> petMap = new HashMap<>();
     
     public static void load(){
-        Pet pet;
-       try(ObjectInputStream readFile = new ObjectInputStream(new FileInputStream(petFile))){
-        try{
-            while(true){
-            pet = (Pet) readFile.readObject();
-            petMap.put(pet.getID(),pet);
-            }
-        }catch(EOFException error){
-            }
-        }catch(IOException | ClassNotFoundException error){
-            // error.printStackTrace();
-       }
     }
 
     public static void save(){
