dce1b4b3567bf89f65b16c8ce9d3e2570f2a9b3c
duynpce <a23.thvl03@gmail.com>
2025-11-04 08:55:00 +0700

    implement(PetService):load and save

--- pet_shop_management_system/src/Service/PetService.java
+++ pet_shop_management_system/src/Service/PetService.java
@@ -1,5 +1,11 @@
 package Service;
 
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 
 import Enum.Constant;
@@ -10,12 +16,33 @@ public interface PetService {
     public final String petFile = Constant.PET_FILE.getFileName();
     public static HashMap<Integer,Pet> petMap = new HashMap<>();
     
+   
     public static void load(){
+        Pet pet;
+       try(ObjectInputStream readFile = new ObjectInputStream(new FileInputStream(petFile))){
+        try{
+            while(true){
+            pet = (Pet) readFile.readObject();
+            petMap.put(pet.getID(),pet);
+            }
+        }catch(EOFException error){
+            }
+        }catch(IOException | ClassNotFoundException error){
+            // error.printStackTrace();
+       }
     }
 
-    public static void save(){
-
+   public static void save() {
+    try (ObjectOutputStream writeFile = new ObjectOutputStream(new FileOutputStream(petFile))) {
+        for (Pet pet : petMap.values()) {
+            writeFile.writeObject(pet);
+        }
+        /// ensure all infor is written
+        writeFile.flush();
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+}
 
     public void add();
     public void remove();
